674a562b49c12a0331dec36502dee66f243ba9a2
Nicholas Boffi <nick.boffi@gmail.com>
2025-06-19 20:34:53 -0400

    add default_mnist

--- py/configs/default_mnist.py
+++ py/configs/default_mnist.py
@@ -1,25 +1,18 @@
 """
 Nicholas M. Boffi
-3/13/25
+6/19/25
+
+Basic configuration for MNIST.
 """
 
 import ml_collections
 
-stopgrad_types = ["convex", "lmd", "none"]
-interp_anneals = [True, False]
-anneal_steps_arr = [0, 7500]
 
-
-def get_config(slurm_id: int):
+def get_config(
+    slurm_id: int, dataset_location: str, output_folder: str
+) -> ml_collections.ConfigDict:
     # ensure jax.device_count works (weird issue with importlib)
     import jax
-
-    # convert slurm id into indices
-    ii = slurm_id % len(stopgrad_types)
-    jj = (slurm_id // len(stopgrad_types)) % len(interp_anneals)
-    kk = (slurm_id // (len(stopgrad_types) * len(interp_anneals))) % len(
-        anneal_steps_arr
-    )
 
     # setup overall config
     config = ml_collections.ConfigDict()
@@ -27,61 +20,80 @@ def get_config(slurm_id: int):
     # training config
     config.training = ml_collections.ConfigDict()
     config.training.shuffle = True
-    config.training.conditional = True
+    config.training.conditional = False
+    config.training.loss_type = "velocity"
     config.training.class_dropout = 0.0
-    config.training.stopgrad_type = stopgrad_types[ii]
-    config.training.interp_anneal = interp_anneals[jj]
-    config.training.anneal_steps = anneal_steps_arr[kk]
     config.training.tmin = 0.0
     config.training.tmax = 1.0
     config.training.seed = 42
     config.training.ema_facs = [0.999, 0.9999]
     config.training.ndevices = jax.device_count()
+    config.training.n_nodes = jax.process_count()
 
     # problem config
     config.problem = ml_collections.ConfigDict()
     config.problem.n = 60000
     config.problem.d = 784
     config.problem.target = "mnist"
+    config.problem.image_dims = (1, 28, 28)
+    config.problem.num_classes = 10
     config.problem.dataset_location = "/n/home04/nboffi/datasets"
     config.problem.interp_type = "linear"
     config.problem.base = "gaussian"
     config.problem.gaussian_scale = "adaptive"
 
-    # logging config
-    config.logging = ml_collections.ConfigDict()
-    config.logging.plot_bs = 5
-    config.logging.visual_freq = 2500
-    config.logging.save_freq = 10000
-    config.logging.wandb_project = "fmm_rebuttal"
-    config.logging.wandb_name = f"3_23_25_mnist_sweep_{slurm_id}"
-    config.logging.wandb_entity = "boffi"
-    config.logging.output_folder = (
-        "/n/home04/nboffi/results/lmm/mnist/3_23_25_mnist_sweep/"
-    )
-    config.logging.output_name = f"3_23_25_mnist_sweep_{slurm_id}"
-
     # optimization config
     config.optimization = ml_collections.ConfigDict()
-    config.optimization.n_epochs = 10000
     config.optimization.bs = 128
     config.optimization.learning_rate = 1e-3
     config.optimization.clip = 5.0
-    config.optimization.decay_steps = 500000
+    config.optimization.total_steps = 500_000
+    config.optimization.total_samples = (
+        config.optimization.bs * config.optimization.total_steps
+    )
+    config.optimization.decay_steps = 500_000
+    config.optimization.schedule_type = "cosine"
+
+    # logging config
+    config.logging = ml_collections.ConfigDict()
+    config.logging.plot_bs = 25000
+    config.logging.visual_freq = 2500
+    config.logging.save_freq = config.optimization.total_steps // 50
+    config.logging.wandb_project = "jax-interpolants-debug"
+    config.logging.wandb_name = f"mnist-debug"
+    config.logging.wandb_entity = "boffi"
+    config.logging.output_folder = output_folder
+    config.logging.output_name = config.logging.wandb_name
+
+    # network config
+    config.network = ml_collections.ConfigDict()
+    config.network.network_type = "mlp"
+    config.network.n_hidden = 4
+    config.network.n_neurons = 256
+    config.network.output_dim = 2
+    config.network.act = "gelu"
+    config.network.use_residual = False
 
     # network config
     config.network = ml_collections.ConfigDict()
     config.network.network_type = "edm2"
     config.network.load_path = ""
-    config.network.img_resolution = 28
-    config.network.img_channels = 1
-    config.network.label_dim = 10
+    config.network.reset_optimizer = False
+    config.network.img_resolution = config.problem.image_dims[1]
+    config.network.img_channels = config.problem.image_dims[0]
+    config.network.input_dims = config.problem.image_dims
+    config.network.label_dim = (
+        config.problem.num_classes if config.training.conditional else 0
+    )
+    config.network.use_cfg = False
     config.network.logvar_channels = 128
+    config.network.use_bfloat16 = False
     config.network.unet_kwargs = {
         "model_channels": 128,
         "channel_mult": [1, 2, 2],
-        "num_blocks": 2,
+        "num_blocks": 3,
         "attn_resolutions": [7],
+        "use_fourier": False,
     }
 
     return config
